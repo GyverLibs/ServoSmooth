a109d08d8180472401fd11991362929f2c71614d
Alex <84599917+GyverLibs@users.noreply.github.com>
2022-06-13 11:59:34 +0300

    upd

--- src/ServoDriverSmooth.h
+++ src/ServoDriverSmooth.h
@@ -5,10 +5,8 @@
 #include "smoothUtil.h"
 #include <Wire.h>
 
-#define _DRIVER_DEADZONE 5			// мёртвая зона (по микросекундам)
-#define _DRIVER_DEADZONE_SP 1		// мёртвая зона (по скорости)
-
-// полное описание библиотеки смотри в ServoSmooth.h
+#define _DRIVER_DEADZONE 5      // мёртвая зона (по микросекундам)
+#define _DRIVER_DEADZONE_SP 1   // мёртвая зона (по скорости)
 
 class ServoDriverSmooth : public Smooth {
 public:
--- src/ServoSmooth.h
+++ src/ServoSmooth.h
@@ -55,48 +55,14 @@
     v3.8 - Исправлен невозврат тика при autoDetach(false)
 */
 
-/*
-    // =========== НАСЛЕДУЕМЫЕ МЕТОДЫ ==========
-    void write(uint16_t angle);					// повернуть на угол. Аналог метода из библиотеки Servo
-    void writeMicroseconds(uint16_t angle);		// повернуть на импульс. Аналог метода из библиотеки Servo
-    void attach();	
-    void attach(int pin, int target);			// аттач + установка позиции (в градусах ИЛИ в микросекундах, программа сама поймёт)
-    void attach(int pin, int min, int max, int target = 0);	// аналог метода из библиотеки Servo. min по умолч. 500, max 2400. target - положение (в углах или мкс, на которые серво повернётся при подключении)
-    void start();								// attach + разрешает работу tick
-    void stop();								// detach + запрещает работу tick
-    
-    boolean tick();								// метод, управляющий сервой, должен опрашиваться как можно чаще.
-    // Возвращает true, когда целевая позиция достигнута.
-    // Имеет встроенный таймер с периодом SS_SERVO_PERIOD
-    
-    boolean tickManual();						// метод, управляющий сервой, без встроенного таймера.
-    // Возвращает true, когда целевая позиция достигнута
-    
-    void setSpeed(int speed);					// установка максимальной скорости (больше 0), градусов / с
-    void setAccel(float accel);					// установка ускорения (0.1-1), условные величины. При 0 - постоянная скорость
-    void setTarget(int target);					// установка целевой позиции в мкс (~500 - 2400 серво, ~150-600 драйвер PCA9685)
-    void setTargetDeg(int target);				// установка целевой позиции в градусах (0-макс. угол). Зависит от min и max
-    void setAutoDetach(boolean set);			// вкл/выкл автоматического отключения (detach) при достижении угла. По умолч. вкл
-    void setCurrent(int target);				// установка текущей позиции в мкс (~500 - 2400 серво, ~150-600 драйвер PCA9685)
-    void setCurrentDeg(int target);				// установка текущей позиции в градусах (0-макс. угол). Зависит от min и max
-    void setMaxAngle(int maxAngle);				// установка макс. угла привода
-    int getCurrent();							// получение текущей позиции в мкс (~500 - 2400 серво, ~150-600 драйвер PCA9685)
-    int getCurrentDeg();						// получение текущей позиции в градусах (0-макс. угол). Зависит от min и max
-    int getTarget();							// получение целевой позиции в мкс (~500 - 2400 серво, ~150-600 драйвер PCA9685)
-    int getTargetDeg();							// получение целевой позиции в градусах (0-макс. угол). Зависит от min и max
-    
-    void setDirection(bool _dir);				// смена направления поворота
-    void smoothStart();							// вызывать сразу после attach(пин, таргет). Смягчает движение серво из неизвестной позиции к стартовой. БЛОКИРУЮЩАЯ!
-*/
-
-#ifndef ServoSmooth_h
-#define ServoSmooth_h
+#ifndef _ServoSmooth_h
+#define _ServoSmooth_h
 #include <Arduino.h>
 #include <Servo.h>
 #include "smoothUtil.h"
 
-#define _SERVO_DEADZONE 10			// мёртвая зона (по микросекундам)
-#define _SERVO_DEADZONE_SP 3		// мёртвая зона (по скорости)
+#define _SERVO_DEADZONE 10      // мёртвая зона (по микросекундам)
+#define _SERVO_DEADZONE_SP 3    // мёртвая зона (по скорости)
 
 class ServoSmooth : public Smooth {
 public:
